b539a49232e3df5f7a619dce48eee9521f0900d7
Adam Norris <anorris@rivasolutionsinc.com>
2021-12-11 14:12:15 -0500

    Added stack to secret name

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
 }
 
 resource "aws_secretsmanager_secret" "mariadb_root" {
-  name                    = "mariadb_root"
+  name                    = "mariadb_root-${var.stack}"
   recovery_window_in_days = 0
 }
 
